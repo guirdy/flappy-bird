0f5ec9968fc8c7c314138e6baf286f60cafb0aab
Guilherme <guilherme.gl1997@gmail.com>
2020-07-06 20:19:02 -0300

    fix: obstacle does not appear

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -40,5 +40,36 @@ function BarrierPair(barrierHeight, barrierOpening, x) {
     this.setX(x);
 }
 
-const obstacle = new BarrierPair(700, 400, 100);
-document.querySelector('[wm-flappy]').appendChild(obstacle.element);
+function Barriers(barriersHeight, playWidth, barriersOpening, space, pointNotification) {
+    this.pairs = [
+        new BarrierPair(barriersHeight, barriersOpening, playWidth),
+        new BarrierPair(barriersHeight, barriersOpening, playWidth + space),
+        new BarrierPair(barriersHeight, barriersOpening, playWidth + space * 2),
+        new BarrierPair(barriersHeight, barriersOpening, playWidth + space * 3)
+    ]
+
+    const displacement = 3;
+    this.animation = () => {
+        this.pairs.forEach(pair => {
+            pair.setX(pair.getX() - displacement);
+
+            if (pair.getX() < -pair.getWidth()) {
+                pair.setX(pair.getX() + space * this.pairs.length);
+                pair.drawOpening();
+            }
+
+            const middle = playWidth / 2;
+            const crossedTheMiddle = pair.getX() + displacement >= middle &&
+                pair.getX() < middle;
+
+            if (crossedTheMiddle) pointNotification();
+        });
+    }
+}
+
+const barriers = new Barriers(800, 800, 520, 400);
+const gameArea = document.querySelector('[wm-flappy]');
+barriers.pairs.forEach(pair => gameArea.appendChild(pair.element));
+setInterval(() => {
+    barriers.animation();
+}, 20);
